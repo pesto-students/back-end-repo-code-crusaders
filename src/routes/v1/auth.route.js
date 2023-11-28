48bb7f8d1d1fffbd260fbf78bcbcb66a66bb6365
krushit dudhat <krushitdudhat2001@gmail.com>
2023-11-28 21:51:46 +0530

    add: doctor register

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -6,7 +6,7 @@ const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/register', validate(authValidation.register), authController.register);
+router.post('/doctor/register', validate(authValidation.registerDoctor), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 
