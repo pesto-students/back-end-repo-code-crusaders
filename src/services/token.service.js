9e5f9a537082a8f5fa5bbb3f0e2a16186d803f77
krushit dudhat <krushitdudhat2001@gmail.com>
2023-12-09 18:46:40 +0530

    fix: remove user service and docker files

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const httpStatus = require('http-status');
 const config = require('../config/config');
-const userService = require('./user.service');
-const { Token } = require('../models');
+const { Token, User } = require('../models');
 const ApiError = require('../utils/ApiError');
 const { tokenTypes } = require('../config/tokens');
 
@@ -91,7 +90,7 @@ const generateAuthTokens = async (user) => {
  * @returns {Promise<string>}
  */
 const generateResetPasswordToken = async (email) => {
-  const user = await userService.getUserByEmail(email);
+  const user = await User.getOne({ email });
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, 'No users found with this email');
   }
